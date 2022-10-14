a9c9235a53d72e8e90c661659f7610158ceca2cc
zeezaglobal <96932409+zeezaglobal@users.noreply.github.com>
2022-10-14 00:59:54 -0700

    Modal class created Modal class Cocktail is created with name and instruction as the variables

---
+++ AppliedActivity1/Models/Cocktail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedActivity1.Models
+{
+    internal class Cocktail
+    {
+        public string Name { get; set; }
+        public string Instructions { get; set; }
+       
+
+        public Cocktail(string name, string instructions)
+        {
+            Name = name;
+            Instructions = instructions;
+          
+        }
+    }
+}
+
+
